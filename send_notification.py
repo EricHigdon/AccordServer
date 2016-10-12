0e73e3d0dd1f110713153e2b3a2af95d33655c6a
Eric Higdon <ehigdon@answersingenesis.org>
2016-10-12 09:50:25 -0400

    Adding send_notification script

---
+++ send_notification.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import os
+import sys
+import django
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+    django.setup()
+    from push_notifications.models import APNSDevice
+    devices = APNSDevice.objects.all()
+    devices.send_message(sys.argv[1], sound='default')
