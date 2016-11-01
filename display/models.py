af215f3396696d3f643cd487da759938c1bf0581
Eric Higdon <ehigdon@answersingenesis.org>
2016-11-01 11:01:31 -0400

    Adding scheduled slides to display

--- display/models.py
+++ display/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
+from bulletin.models import Church
 
 upload_storage = FileSystemStorage(location=settings.UPLOAD_URL)
 # Create your models here.
 
 class Slide(models.Model):
+    church = models.ForeignKey(Church, related_name='slides')
     name = models.CharField(max_length=200, unique=True)
     image = models.FileField(storage=upload_storage, blank=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(blank=True, null=True)
     
     def __str__(self):
         return self.name
--- display/views.py
+++ display/views.py
@@ -1,11 +1,25 @@
-from .models import Slide
 from rest_framework import viewsets
 from .serializers import SlideSerializer
-
+from django.shortcuts import get_object_or_404
+from bulletin.models import Church
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.utils import timezone
+from django.db.models import Q
 
 class SlideViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows slides to be viewed.
     """
-    queryset = Slide.objects.all()
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+    queryset = Church.objects.all()
     serializer_class = SlideSerializer
+
+    def get_queryset(self):
+        church = get_object_or_404(Church, admins=self.request.user)
+        now = timezone.now()
+        return church.slides.filter(
+            Q(start_date__lte=now, end_date__gte=now) |
+            Q(start_date__lte=now, end_date__isnull=True)
+        ).all()
