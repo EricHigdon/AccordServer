dae9a34fee9c792bbff205a3117df09678cb43cc
Eric Higdon <ehigdon@answersingenesis.org>
2017-05-26 09:17:49 -0400

    use DRF authentication for modified api view

--- bulletin/urls.py
+++ bulletin/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^api/(?P<church_pk>[0-9]+)/$', views.api, name='api'),
-    url(r'^modified/(?P<church_pk>[0-9]+)/$', views.modified, name='modified'),
+    url(r'^modified/(?P<church_pk>[0-9]+)/$', views.ModifiedAPI().as_view(), name='modified'),
     url(r'^form/(?P<form_id>[0-9]+)/$', views.form, name='form'),
     url(r'^account/$', csrf_exempt(views.UserViewSet.as_view({'get': 'list', 'put': 'create', 'post': 'update'})), name='account'),
 ]
